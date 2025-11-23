98b5ec8ef8abeac9093597ce7a2047b94d28abb0
Patrick <patrickpleumann@web.de>
2025-11-23 23:27:35 +0100

    implemented smoothed rotation with mouse x and y axis seperated

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,6 @@
 using System;
+using Unity.VisualScripting;
+using UnityEditor.Search;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
@@ -11,70 +13,105 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float maxSpeed;
     [SerializeField] private float jumpForce;
 
-    [SerializeField] private InputActionReference move;
-    [SerializeField] private InputActionReference look;
-    [SerializeField] private InputActionReference jump;
-    [SerializeField] private InputActionReference shoot;
+    [SerializeField] public InputActionReference shoot;
+    [SerializeField] public InputActionReference move;
+    [SerializeField] public InputActionReference look;
+    [SerializeField] public InputActionReference jump;
 
-    [SerializeField] private float gravityScale = 1f;
-    [SerializeField] private float gravityForce = 9.81f;
+    [SerializeField] public float gravityScale = 1f;
+    [SerializeField] public float gravityForce = 9.81f;
+    [SerializeField] public float cameraClampPos = 89f;
+    [SerializeField] public float cameraClampNeg = -89;
+    [SerializeField] public float cameraSensitivity = 20f;
 
-    [SerializeField] private float cameraClampPos = 89f;
-    [SerializeField] private float cameraClampNeg = -89;
-    [SerializeField] private float cameraSensitivity = 20f;
     [SerializeField] private Transform cameraTransform;
+    [SerializeField] private Transform playerTransform; // because I wanted to seperate rb rotation from camera rotation
+    [SerializeField] private Transform gunBarrelRoot;
 
-    private Rigidbody rb;
-    private Vector2 lookDir;
+    private Rigidbody rb_Body;
+
+    //SmoothDamping
+    private Vector3 refVector = Vector3.zero;
+
+    //Directions
+    private Vector2 cameraDir;
+    private Vector2 bodyDir;
+    private Vector2 moveDir;
+
+    //Rotations
+    [SerializeField] private float smoothTime = 25f;
+    private Vector3 eulerAngleRotation;
+    private Vector3 smoothRotation;
+
+    //Velocities
+    private Vector3 currentVel;
+    private Vector3 targetVel;
+    private Vector3 velocityChange;
+
 
 
     private void OnEnable()
     {
-        shoot.action.started += OnShoot;
+        //shoot.action.started += OnShoot;
         jump.action.started += OnJump;
+        look.action.started += OnLook;
+    }
+
+    private void OnLook(InputAction.CallbackContext context)
+    {
+        UpdateBodyRotation();
+        UpdateCameraRotation();
     }
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        rb_Body = GetComponent<Rigidbody>();
     }
 
     private void OnJump(InputAction.CallbackContext context)
     {
-        rb.AddForce(new Vector3(0, 1 * jumpForce, 0));
+        rb_Body.AddForce(new Vector3(0, 1 * jumpForce, 0));
     }
 
-    private void OnShoot(InputAction.CallbackContext context)
-    {
-        throw new NotImplementedException();
-    }
+    //private void OnShoot(InputAction.CallbackContext context)
+    //{
+    //    throw new NotImplementedException();
+    //}
 
     private void MovePlayer()
     {
-        var moveDirection = move.action.ReadValue<Vector2>();
+        moveDir = move.action.ReadValue<Vector2>();
 
-        Vector3 currentVelocity = rb.linearVelocity;
-        Vector3 targetVelocity = new Vector3(moveDirection.x, 0f, moveDirection.y);
-        targetVelocity *= maxSpeed;
+        currentVel = rb_Body.linearVelocity;
+        targetVel = new Vector3(moveDir.x, 0f, moveDir.y);
+        targetVel *= maxSpeed;
 
-        targetVelocity = transform.TransformDirection(targetVelocity);
+        targetVel = playerTransform.TransformDirection(targetVel);
 
-        Vector3 velocityChange = targetVelocity - currentVelocity;
+        velocityChange = targetVel - currentVel;
         velocityChange.y = 0f;
         velocityChange = Vector3.ClampMagnitude(velocityChange, maxSpeed);
 
-        rb.AddForce(velocityChange, ForceMode.VelocityChange);
+        rb_Body.AddForce(velocityChange, ForceMode.VelocityChange);
     }
 
-    public void UpdateRotation()
+    public void UpdateBodyRotation()
     {
-        var rotation = look.action.ReadValue<Vector2>();
-        var currentRotation = cameraTransform.rotation.eulerAngles;
-        var targetRotation = currentRotation + new Vector3(rotation.y, rotation.x, 0f);
+        bodyDir = look.action.ReadValue<Vector2>();
+        
+        eulerAngleRotation.y += (bodyDir.x  * cameraSensitivity * Time.deltaTime);
 
-        Mathf.Clamp(cameraTransform.rotation.x, 0, cameraClampPos);
-        Mathf.Clamp(cameraTransform.rotation.y, 0, cameraClampNeg);
-        cameraTransform.rotation = Quaternion.Euler(targetRotation);
+        smoothRotation.y = Mathf.Lerp(smoothRotation.y, eulerAngleRotation.y, smoothTime * Time.deltaTime);
+    }
+
+    public void UpdateCameraRotation()
+    {
+        cameraDir = look.action.ReadValue<Vector2>();
+
+        eulerAngleRotation.x += -cameraDir.y * cameraSensitivity * Time.deltaTime;
+        eulerAngleRotation.x = Mathf.Clamp(eulerAngleRotation.x, -89f, 90f);
+
+        smoothRotation.x = Mathf.Lerp(smoothRotation.x, eulerAngleRotation.x, smoothTime * Time.deltaTime);
     }
 
 
@@ -85,6 +122,13 @@ public class PlayerController : MonoBehaviour
 
     private void LateUpdate()
     {
-        UpdateRotation();
+
+        cameraTransform.eulerAngles = smoothRotation;
+        rb_Body.rotation = Quaternion.Euler(0f,smoothRotation.y,0f);
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawRay(gunBarrelRoot.position, gunBarrelRoot.forward);
     }
 }
